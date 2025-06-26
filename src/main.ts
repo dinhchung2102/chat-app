d44c3298514cbcc19808634540e93ba31089ed57
dinhchung2102 <vodinhchung4@gmail.com>
2025-06-26 16:19:23 +0700

    fix: enable cors

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
   app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
   // main.ts
   app.enableCors({
-    origin: ['http://localhost:5173, http://localhost:3000'],
+    origin: ['http://localhost:5173', 'http://localhost:3000'],
     credentials: true,
   });
 
